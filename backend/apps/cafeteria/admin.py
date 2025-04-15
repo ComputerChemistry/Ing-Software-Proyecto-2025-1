29bb128d343309986ed630e5c98b5071f2b16491
ComputerChemistry <carloseduardomorenovargas02@gmail.com>
2025-04-14 21:42:56 -0700

    SCRUM-6 API Rest Agregada al Proyecto

--- backend/apps/cafeteria/admin.py
+++ backend/apps/cafeteria/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Menus,Tienditas,Facultades,Usuarios
+admin.site.register(Menus)
+admin.site.register(Tienditas)
+admin.site.register(Facultades)
+admin.site.register(Usuarios)
 
 # Register your models here.
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
-    path('polls/', include("polls.urls")),
+    path('cafeteria/', include('apps.cafeteria.urls')),
     path('admin/', admin.site.urls),  
+    path('', RedirectView.as_view(url='/cafeteria/', permanent=False)),  
 ]
