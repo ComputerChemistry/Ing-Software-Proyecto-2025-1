4d4f93d675b2ddb72781961122c8160a0641c5f7
ComputerChemistry <carloseduardomorenovargas02@gmail.com>
2025-04-17 03:16:28 -0700

    Nuevas carpetas de components, utils y pages

---
+++ frontend/ingenieria-software-frontend/src/components/CafeteriaCard.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+type Cafeteria = {
+    
+    name: string; 
+    location: string:
+    imageURL: string;
+    
+}
+
+const CafeteriaCard: React.FC<Cafeteria> = ({name, location, imageURL}) => {
+
+    return (
+	
+	<div= className: 
+	    <img= className:
+	    <div= className:
+	    <h2= className:
+	    <p= className:
+
+	
+
+    );
+};
+
