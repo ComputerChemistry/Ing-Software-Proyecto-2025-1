bcd7bc3bfbd5188b5061392d5176c038fd12c1a6
Joaco2025 <joaquinalf177@gmail.com>
2025-04-23 17:07:03 -0700

    Aqui esta pa que dejes de chingar joto

---
+++ frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import Login from './Login.jsx';
+import './index.css';
+import App from './App.jsx'
+
+/*ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>
+)
+*/
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
